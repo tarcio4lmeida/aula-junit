0c9d4698eff4c2b730325bd0759ec352ee13e844
tarcio almeida <tarcioalmeidalima@gmail.com>
2020-12-18 00:06:42 -0300

    Teste saque total deveria limpar e retornar o total saldo

--- src/tests/entities/AccountTests.java
+++ src/tests/entities/AccountTests.java
@@ -29,4 +29,16 @@ public class AccountTests {
 		
 		Assertions.assertEquals(expectedValue, acc.getBalance());
 	}
+	
+	@Test
+	public void fullWithdrawShouldClearBalanceAndReturnFullBalance() {
+		double expectedValue = 0.0;
+		double initialBalance = 800.0;
+		Account acc = AccountFactory.createAccount(initialBalance);
+		
+		double result = acc.fullWithdraw();
+		
+		Assertions.assertTrue(expectedValue == acc.getBalance());
+		Assertions.assertEquals(result, initialBalance);
+	}
 }
